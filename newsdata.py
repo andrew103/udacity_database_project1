393938274560e5cc5af71a3d1318daba99ec1475
Andrew Euredjian <andrew_103@rocketmail.com>
2017-06-30 16:44:02 -0700

    reworked to meet PEP-8 requirements

--- newsdata.py
+++ newsdata.py
@@ -7,8 +7,10 @@ def popular_article():
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
 
-    # use slug to relate IP views to articles: where log.path = '%'articles.slug
-    c.execute("""select articles.title, articles.author, cast(count(articles.slug) as int) as num
+    # use slug to relate IP views to articles:
+    # where log.path = '%'articles.slug
+    c.execute("""select articles.title, articles.author,
+                        cast(count(articles.slug) as int) as num
                     from articles, log
                     where log.path like concat('%', articles.slug)
                     group by articles.title, articles.author
@@ -17,17 +19,23 @@ def popular_article():
     return c.fetchall()
     db.close()
 
+
 def popular_author():
     """ Finds and lists the authors and their number of views from greatest
     to least """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
 
-    # use double select statement with inner being the select statement from the popular_article function
-    c.execute("""select authors.name, cast(sum(article_views.num) as int) as num
-                    from authors, (select articles.title, articles.author, cast(count(articles.slug) as int) as num
+    # use double select statement with inner being the select statement
+    # from the popular_article function
+    c.execute("""select authors.name,
+                        cast(sum(article_views.num) as int) as num
+                    from authors, (select articles.title,
+                                    articles.author,
+                                    cast(count(articles.slug) as int) as num
                                     from articles, log
-                                    where log.path like concat('%', articles.slug)
+                                    where log.path
+                                        like concat('%', articles.slug)
                                     group by articles.title, articles.author
                                     order by num desc) as article_views
                     where authors.id = article_views.author
@@ -37,19 +45,24 @@ def popular_author():
     return c.fetchall()
     db.close()
 
+
 def percent_error_requests():
     """ Analyzes the site logs to determine days where a percentage of traffic
     receiving error messages such as 404 NOT FOUND that exceed 1% """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
 
-    # find a way to pull status messages given the same day then do math for avg
-    c.execute("""select a.date, concat(cast((b.num * 100.0/(a.num + b.num))as float), '%')
-                    from (select date(time) as date, log.status, count(log.status) as num
+    # find a way to pull status messages given the same day
+    # then do math for avg
+    c.execute("""select a.date,
+                    concat(cast((b.num * 100.0/(a.num + b.num))as float), '%')
+                    from (select date(time) as date,
+                                log.status, count(log.status) as num
                             from log
                             group by date(time), log.status
                             order by date(time) desc) as a,
-                         (select date(time) as date, log.status, count(log.status) as num
+                         (select date(time) as date,
+                                log.status, count(log.status) as num
                             from log
                             group by date(time), log.status
                             order by date(time) desc) as b
@@ -61,6 +74,7 @@ def percent_error_requests():
 
     return c.fetchall()
     db.close()
+
 
 # Main function that runs all or some functions in the file
 def run():
