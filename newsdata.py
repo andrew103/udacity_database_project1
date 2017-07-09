d017be24a31d6755e1645f3b6faa3e316794755a
Andrew Euredjian <andrew_103@rocketmail.com>
2017-07-09 13:57:08 -0700

    implemented changes recommended by udacity review

--- newsdata.py
+++ newsdata.py
@@ -1,88 +1,88 @@
 import psycopg2
 
 
-def popular_article():
-    """ Finds and lists the articles and their number of views from greatest
-    to least """
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+# Variables that hold queries
+article_query = """
+select articles.title, articles.author, cast(count(articles.slug)as int) as num
+    from articles, log
+    where log.path like concat('%', articles.slug)
+    group by articles.title, articles.author
+    order by num desc;
+"""
 
-    # use slug to relate IP views to articles:
-    # where log.path = '%'articles.slug
-    c.execute("""select articles.title, articles.author,
-                        cast(count(articles.slug) as int) as num
+author_query = """
+select authors.name, cast(sum(article_views.num) as int) as num
+    from authors, (select articles.title, articles.author,
+                                cast(count(articles.slug) as int) as num
                     from articles, log
                     where log.path like concat('%', articles.slug)
                     group by articles.title, articles.author
-                    order by num desc;""")
+                    order by num desc) as article_views
+    where authors.id = article_views.author
+    group by authors.name
+    order by num desc;
+"""
 
-    return c.fetchall()
+error_query = """
+select a.date, concat(cast((b.num * 100.0/(a.num + b.num))as float), '%')
+    from (select date(time) as date, log.status, count(log.status) as num
+                from log
+                group by date(time), log.status
+                order by date(time) desc) as a,
+         (select date(time) as date, log.status, count(log.status) as num
+                from log
+                group by date(time), log.status
+                order by date(time) desc) as b
+    where a.date = b.date
+        and a.status != b.status
+        and a.status = '200 OK'
+        and cast((b.num * 100.0/(a.num + b.num))as float) > 1.0
+    order by a.date desc;
+"""
+
+
+
+
+def execute_query(query):
+    """ Dedicated function for running queries that are passed in """
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+
+    c.execute(query)
+
+    result = c.fetchall()
     db.close()
+    return result
+
+
+def popular_article():
+    """ Finds and lists the articles and their number of views from greatest
+    to least """
+    # use slug to relate IP views to articles:
+    # where log.path = '%'articles.slug
+    return execute_query(article_query)
 
 
 def popular_author():
     """ Finds and lists the authors and their number of views from greatest
     to least """
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-
     # use double select statement with inner being the select statement
     # from the popular_article function
-    c.execute("""select authors.name,
-                        cast(sum(article_views.num) as int) as num
-                    from authors, (select articles.title,
-                                    articles.author,
-                                    cast(count(articles.slug) as int) as num
-                                    from articles, log
-                                    where log.path
-                                        like concat('%', articles.slug)
-                                    group by articles.title, articles.author
-                                    order by num desc) as article_views
-                    where authors.id = article_views.author
-                    group by authors.name
-                    order by num desc;""")
-
-    return c.fetchall()
-    db.close()
+    return execute_query(author_query)
 
 
 def percent_error_requests():
     """ Analyzes the site logs to determine days where a percentage of traffic
     receiving error messages such as 404 NOT FOUND that exceed 1% """
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-
     # find a way to pull status messages given the same day
     # then do math for avg
-    c.execute("""select a.date,
-                    concat(cast((b.num * 100.0/(a.num + b.num))as float), '%')
-                    from (select date(time) as date,
-                                log.status, count(log.status) as num
-                            from log
-                            group by date(time), log.status
-                            order by date(time) desc) as a,
-                         (select date(time) as date,
-                                log.status, count(log.status) as num
-                            from log
-                            group by date(time), log.status
-                            order by date(time) desc) as b
-                    where a.date = b.date
-                    and a.status != b.status
-                    and a.status = '200 OK'
-                    and cast((b.num * 100.0/(a.num + b.num))as float) > 1.0
-                    order by a.date desc;""")
-
-    return c.fetchall()
-    db.close()
+    return execute_query(error_query)
 
 
 # Main function that runs all or some functions in the file
-def run():
+if __name__ == "__main__":
     print(popular_article())
     print('')
     print(popular_author())
     print('')
     print(percent_error_requests())
-
-
-run()
